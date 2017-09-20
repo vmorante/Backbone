14d3d5b7c43a619705a6ee211d193b803e29aa04
Veronica <veronica.morante.pindador@gmail.com>
2017-09-20 15:29:20 +0200

    Eventos

--- js/main.js
+++ js/main.js
@@ -1,9 +1,22 @@
-var persn = {
+var person = {
     name: "Mosh",
     walk: function() {
-        this.trigger("walking")
+        this.trigger("walking", {
+            speed: 1,
+            startTime: "08:00"
+        })
     }
 };
 
 
-_.extend(person, Backbonde.Events);
+_.extend(person, Backbone.Events);
+
+
+person.once("walking", function(e) {
+    console.log("Person is walking");
+    console.log("Event Args", e);
+});
+
+
+person.walk();
+person.walk();
