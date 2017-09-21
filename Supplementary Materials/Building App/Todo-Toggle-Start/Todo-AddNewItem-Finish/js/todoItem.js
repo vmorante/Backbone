70f8d4324c7911efda3b02f464e07ca534ae3148
Veronica <veronica.morante.pindador@gmail.com>
2017-09-21 16:59:09 +0200

    add toggle

--- Supplementary Materials/Building App/Todo-Toggle-Start/Todo-AddNewItem-Finish/js/todoItem.js
+++ Supplementary Materials/Building App/Todo-Toggle-Start/Todo-AddNewItem-Finish/js/todoItem.js
@@ -1,7 +1,13 @@
-
 var TodoItem = Backbone.Model.extend({
-	validate: function(attrs){
-		if (!attrs.description)
-			return "Description is required.";
-	}
+    defaults: {
+        isCompleted: false
+    },
+    validate: function(attrs) {
+        if (!attrs.description)
+            return "Description is required.";
+    },
+    toggle: function() {
+        this.set("isCompleted", !this.get("isCompleted"))
+
+    }
 });
--- Supplementary Materials/Building App/Todo-Toggle-Start/Todo-AddNewItem-Finish/js/todoItemView.js
+++ Supplementary Materials/Building App/Todo-Toggle-Start/Todo-AddNewItem-Finish/js/todoItemView.js
@@ -1,15 +1,28 @@
-
 var TodoItemView = Backbone.View.extend({
-	tagName: "li",
-	
-	initialize: function(options){
-		if (!(options && options.model))
-			throw new Error("model is not specified.");
-	},
+    tagName: "li",
 
-	render: function(){
-		this.$el.html(this.model.escape("description"));
+    initialize: function(options) {
+        if (!(options && options.model))
+            throw new Error("model is not specified.");
+        this.model.on("change", this.render, this)
+    },
+    events: {
+        "click #toggle": "onClickToggle"
+    },
 
-		return this;
-	}
+    onClickToggle: function() {
+
+
+        this.model.toggle();
+        console.log(this.model.toJSON());
+    },
+
+    render: function() {
+        this.$el.toggleClass("completed", this.model.get("isCompleted"))
+
+        var checked = this.model.get("isCompleted") ? "checked" : "";
+        this.$el.html("<input id='toggle' type='checkbox'" + checked + "></input>" + this.model.escape("description"));
+
+        return this;
+    }
 });
